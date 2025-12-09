b092664fe4bbd795769ae3edf2f95ff70ab8c258
Muthulaksmi Viswanathan <muthuvd2@gmail.com>
2025-12-09 10:03:12 -0500

    changed

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
 console.log("world");
+//Connect local with remote repository
+//check github2 slide number 12 for comands 
+//git init
+//touch README.md    or   echo. > README.md     (create a README file)
+//git add .                                                                                    (Stage)
+//git commit -m "Initial commit"                                      (commit)
+//git branch -M main					   (ensures current branch is main)
+//git remote add origin git@github.com:your-username/repository-name.git
+//(link your local Git repository to a remote repository)
+//git push origin main                                                             (push changes to GitHub)
